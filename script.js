1de0059341102016345ad74882d18a464b92b15f
Chisato-Tct <c-ando@tech-comms.jp>
2026-02-04 18:28:51 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function calculate() {
+  const count = Number(document.getElementById("count").value);
+  const thread = Number(document.getElementById("thread").value);
+  const silk = Number(document.getElementById("silk").value);
+
+  const totalThread = count * thread;
+  const totalSilk = count * silk;
+
+  document.getElementById("result").innerHTML = `
+    <h3>計算結果</h3>
+    <p>必要な糸：${totalThread}</p>
+    <p>必要な絹：${totalSilk}</p>
+  `;
+}
