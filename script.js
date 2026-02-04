0f873125a0ee6a62e10ca82a61adca018caff50b
Chisato-Tct <c-ando@tech-comms.jp>
2026-02-04 18:38:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-alert("JSは生きてる");
+console.log("JS loaded");
+
 function calculate() {
   const count = Number(document.getElementById("count").value);
   const thread = Number(document.getElementById("thread").value);
@@ -7,9 +8,7 @@ function calculate() {
   const totalThread = count * thread;
   const totalSilk = count * silk;
 
-  document.getElementById("result").innerHTML = `
-    <h3>計算結果</h3>
-    <p>必要な糸：${totalThread}</p>
-    <p>必要な絹：${totalSilk}</p>
-  `;
+  document.getElementById("result").innerHTML =
+    "必要な糸：" + totalThread + "<br>" +
+    "必要な絹：" + totalSilk;
 }
